cee62fd02d81df01891351752a9339f3557e7ca4
Jake Filipowski <filipowskijm@gmail.com>
2023-07-15 10:35:49 -0400

    added state/todo add/delete functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (todo !== "") {
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
         <h1>To-Do List</h1>
-        <form>
-          <label>Task:</label>
-          <input type="text"></input>
-          <input type="submit"></input>
+      <div>
+      <form>
+            <label>Task:</label>
+                <input 
+                type="text" 
+                name="todo"
+                value={todo}
+                placeholder="Enter a Task..." 
+                onChange={(e) => {
+                  setTodo(e.target.value);
+                }}>
+                </input>
+            <input type="submit" onClick={addTodo}></input>
         </form>
-      </header>
+      </div>
+      {todos?.length > 0 ? (
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <div className="todo">
+              <li key={index}> {todo} </li>
+
+              <button 
+              className="delete-button"
+              onClick={() => {
+                deleteTodo(todo);
+              }}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <p>No task found</p>
+        </div>
+      )}
     </div>
   );
 }
